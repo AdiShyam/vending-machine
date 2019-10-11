15c8dde1e88f6cea8e8936b72307ca991f637da9
Shyam <bshyam@visteon.com>
2019-10-11 10:50:01 +0530

    vending machine initial directory structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Honey well products
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mock-data/product.js
@@ -0,0 +1,32 @@
+const listOfProducts = [
+    {
+        id: 1,
+        name: "Five Star",
+        price: 5,
+        quantity: 20
+    },
+    {
+        id: 2,
+        name: "Munch",
+        price: 10,
+        quantity: 10
+    },
+    {
+        id: 3,
+        name: "Lays",
+        price: 20,
+        quantity: 15
+    },
+    {
+        id: 4,
+        name: "coke",
+        price: 30,
+        quantity: 55
+    },
+    {
+        id: 5,
+        name: "Thunbs up",
+        price: 84,
+        quantity: 30
+    }
+]
